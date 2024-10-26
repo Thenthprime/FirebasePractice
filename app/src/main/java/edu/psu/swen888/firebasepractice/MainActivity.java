af799c9e0f488b0140065c96e2b2df5e924f7e7f
Thenthprime <122050458+Thenthprime@users.noreply.github.com>
2024-10-26 08:09:36 -0500

    main activity working

---
+++ app/src/main/java/edu/psu/swen888/firebasepractice/MainActivity.java
@@ -0,0 +1,66 @@
+package edu.psu.swen888.firebasepractice;
+
+
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mFireBaseAuth;
+    Button signInButton;
+    Button signUpButton;
+    EditText email;
+    EditText password;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        mFireBaseAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.editTextEmailAddress);
+        password = findViewById(R.id.editTextNumberPassword);
+        signInButton = findViewById(R.id.signInbutton);
+        signUpButton = findViewById(R.id.signUpbutton);
+        signInButton.setOnClickListener(view -> {
+            SignIn(email.getText().toString(), password.getText().toString());
+        });
+        signUpButton.setOnClickListener(view -> {
+
+//            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+//            startActivity(intent);
+        });
+    }
+
+    private void SignIn(String email, String password) {
+        mFireBaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+//                            Intent intent = new Intent(MainActivity.this, BooksActivity.class);
+//                            intent.putExtra("email", email);
+//                            startActivity(intent);
+//                            finish();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Authentication Failed. Please create an Account", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
+
